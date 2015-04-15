4bfc485f26d6aeae112fcd1598f1dcc8f87b3211
Geoffrey <glongsword@hotmail.com>
2015-04-14 23:53:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -76,10 +76,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Allow other domains access
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  return next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // Make our db accessible to our router
